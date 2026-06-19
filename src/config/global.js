9cccb5018b9349e90e46f080d31894a7bb8a56af
acearaujo <159490768+acearaujo@users.noreply.github.com>
2026-06-19 15:44:28 -0500

    CFA

--- src/config/global.js
+++ src/config/global.js
@@ -234,7 +234,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CFA1_83210128_DU.zip',
+        download: 'downloads/83210128_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
